6d7c34bc8d5c832fd42db950e7f92cbaa4aaca01
Stratton Sloane <stratton.s@outlook.com>
2018-12-10 17:28:32 +1100

    Add basic render and update times

--- Debug.cs
+++ Debug.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Wisp.Nodes;
 
 namespace Wisp
-{ 
+{
     public static class Debug
     {
         public static string Output { get; private set; }
@@ -20,6 +18,36 @@ namespace Wisp
         public static void ClearOutput()
         {
             Output = "";
+        }
+
+        public class Sampler
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            List<float> samples = new List<float>();
+
+            public void Start()
+            {
+                stopwatch.Reset();
+                stopwatch.Start();
+            }
+
+            public void End()
+            {
+                stopwatch.Stop();
+                AddSample((float)stopwatch.Elapsed.TotalMilliseconds);
+            }
+
+            public void AddSample(float sample) => samples.Add(sample);
+            
+            public float GetAverage()
+            {
+                float total = 0;
+                foreach (var item in samples) total += item;
+                float average = total / samples.Count;
+                return average;
+            }
+
+            public void Clear() => samples.Clear();
         }
     }
 }
