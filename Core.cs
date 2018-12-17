9a70204b6f990fe79f9405045d417050626a3045
Stratton Sloane <stratton.s@outlook.com>
2018-12-17 11:08:56 +1100

    Move main updating and rendering functions into a new Core class

---
+++ Core.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Wisp
+{
+    public class Core
+    {
+        Game game;
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        SceneRender renderer;
+        SceneManager sceneManager;
+
+        public Color ClearColor { get; set; } = Color.CornflowerBlue;
+
+        public Core(Game game, GraphicsDeviceManager graphics)
+        {
+            this.game = game;
+            this.graphics = graphics;
+
+            renderer = new SceneRender(game);
+            sceneManager = new SceneManager(game, graphics);
+            spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyManager.Update(Keyboard.GetState(), (float)gameTime.ElapsedGameTime.TotalSeconds);
+            sceneManager.UpdateCurrentScenes(gameTime);
+        }
+
+        public void Render()
+        {
+            graphics.GraphicsDevice.Clear(ClearColor);
+            sceneManager.RenderCurrentScenes(spriteBatch);
+        }
+
+        public void AddScene<T>() where T : Scene => sceneManager.AddScene<T>();
+
+        public void SetCurrentScene(SceneType type, string scene) => sceneManager.SetCurrentScene(type, scene);
+
+        public void AddHandler<T>() where T : Component => sceneManager.AddHandler<T>();
+
+        public void AddEventHandler<T>(IEventHandler<T> handler) where T : Event => 
+            sceneManager.AddEventHandler(handler);
+
+        public void SetRenderOrder(IEnumerable<SceneType> order) => sceneManager.SetRenderOrder(order);
+    }
+}
